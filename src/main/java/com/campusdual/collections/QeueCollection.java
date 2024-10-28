430efe5a5ad3bfe2e91c0b3b522a505b1b12c655
Ismael Leon Cal <ileoncal@gmail.com>
2024-10-28 10:00:52 +0100

    28-10-24 III

---
+++ src/main/java/com/campusdual/collections/QeueCollection.java
@@ -0,0 +1,36 @@
+package com.campusdual.collections;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QeueCollection {
+    public static void main(String[] args) {
+        Queue<String> instruments = new LinkedList<>();
+
+        instruments.offer("Guitar");
+        instruments.offer("Piano");
+        instruments.offer("Drums");
+        instruments.offer("Violin");
+
+        System.out.println("Queue of muscial instruments: ");
+        for (String item: instruments){
+            System.out.println(item);
+        }
+
+        String removedInstrument = instruments.poll();
+        System.out.println("\nRemoved: " + removedInstrument);
+
+        System.out.println("\nQueue after removal: ");
+        for (String item: instruments){
+            System.out.println(item);
+        }
+
+        String peekedinstrument = instruments.peek();
+        System.out.println("\nPeeked: " + peekedinstrument);
+
+        System.out.println("\nQueue after peeked: ");
+        for (String item: instruments){
+            System.out.println(item);
+        }
+    }
+}
